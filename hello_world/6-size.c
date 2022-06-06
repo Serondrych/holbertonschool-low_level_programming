b615f2a5ee3173ae4de437457710749e87b755ac
Serondrych <serondrych@gmail.com>
2022-06-06 08:18:25 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Print the size of various types and exit
+*
+* Return: Always (0) (Success)
+*
+*/
+int main(void)
+{
+	char charType;
+	int intType;
+	long longType;
+	long long longlongType;
+	float floatType;
+
+	printf("Size of char: %zu bytes(s)\n", sizeof(charType));
+	printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
