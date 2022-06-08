82394f7a5dc71e3648b3864e024d25bc86a21f3e
Serondrych <serondrych@gmail.com>
2022-06-07 18:02:55 -0700

    1-last_digit.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+		putchar('\n');
+	return (0);
+}
