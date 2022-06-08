a336211f8e98bc33882c3e49353f64d220622224
Serondrych <serondrych@gmail.com>
2022-06-08 06:56:05 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num = 48;
+	int let = 65;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+	while (let < 71)
+	{
+		putchar(let);
+		let++;
+	}
+	putchar(10);
+	return (0);
+}
