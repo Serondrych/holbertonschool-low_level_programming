fc6289781b7a4ef7d69a3b4d9b2d5dc37c9a5cc8
Serondrych <serondrych@gmail.com>
2022-06-09 10:48:35 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
 * print_alphabet - prints the alphabet
-* print_alphabet_x10 - prints the alphabet ten times
+*
 * c: characters
 * i: numbers
 *
@@ -18,6 +18,10 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
+/**
+* print_alphabet_x10 - prints the alphabet ten times
+*
+*/
 
 void print_alphabet_x10(void)
 {
