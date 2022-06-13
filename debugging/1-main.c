b779139a9b5c86cb9de57365433037ed09209d1f
Serondrych <serondrych@gmail.com>
2022-06-13 07:28:25 -0700

    1-main

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -10,15 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-/**
-*	struct - loop
-*
-*	i = 0;
-*	while (i < 10)
-*	{
-*	putchar(i);
-*	}
-*/	printf("Infinite loop avoided! \\o/\n");
+/* Loop starts here */
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
