926c8e0aa91c0744e3480525fb65fa56d0c3de25
Serondrych <serondrych@gmail.com>
2022-06-13 10:41:48 -0700

    11-98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,13 @@
 
 void print_to_98(int n)
 {
-	for (n = 98; n <= 0; n++)
+	for (n = 0; n <= 98; n++)
 	{
 		printf("%d", n);
 		if (n == 98)
 			continue;
 		printf(", ");
-	}
-	
+	}	
 
 	for (; n >= 98; n--)
 	{
