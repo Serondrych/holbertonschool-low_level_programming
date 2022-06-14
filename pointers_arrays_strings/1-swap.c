6078e665c6894b15e266fc05c576553c3174c6d8
Serondrych <serondrych@gmail.com>
2022-06-14 10:01:32 -0700

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* swap_int - swap the vaules of two integers
+* @a: integer 1
+* @b: integer 2
+*
+* Return: Always 0.
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+	
+	*a = d;
+	*b = c;
+}
