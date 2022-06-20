0d5602c3761db3d25586bcaa07c887cc7d24d103
Serondrych <serondrych@gmail.com>
2022-06-19 17:49:05 -0700

    1-strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings
+* @dest: destination
+* @src: source
+* @n: bytes from src
+*
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int l = 0;
+
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[l + i] = src[i];
+	}
+	dest[l + i] = '\0';
+
+	return (dest);
+}
